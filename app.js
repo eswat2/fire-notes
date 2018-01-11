ae4e10305832400b9bd04474ba1afedc77f1f2ea
Richard Hess <eswat2@gmail.com>
2018-01-11 15:37:00 -0800

    add eslintrc, cleanup code

--- app.js
+++ app.js
@@ -1,159 +1,155 @@
-let wsServer = require("ws").Server
-let express  = require('express');
-let http     = require('http');
-let fs       = require('fs');
-let request  = require('request');
-let notes    = require('./notes');
+/* eslint no-console: "off" */
+const WsServer = require('ws').Server
+const express  = require('express')
+const http     = require('http')
+const notes    = require('./notes')
 
-let bodyParser = require('body-parser');
-let cors = require('cors');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-let app  = express();
-let port = process.env.PORT || 5000;
+const app  = express()
+const port = process.env.PORT || 5000
 
-let mock = {
-  wut:'a simple websocket & REST api notes server',
-  why:'wanted to build something like firebase',
-  who:'Richard Hess (aka. eswat2)',
-  app:'https://egghead-notes.herokuapp.com',
-  git:'https://github.com/eswat2/fire-notes',
-  wss:'wss://fire-notes.herokuapp.com',
-  api:[
-    { url:'/keys',       verb:'GET',  what:'list of keys' },
-    { url:'/notes',      verb:'POST', what:'creates/updates a note container' },
-    { url:'/notes/:key', verb:'GET',  what:'fetch note container for this key' },
+const mock = {
+  wut: 'a simple websocket & REST api notes server',
+  why: 'wanted to build something like firebase',
+  who: 'Richard Hess (aka. eswat2)',
+  app: 'https://egghead-notes.herokuapp.com',
+  git: 'https://github.com/eswat2/fire-notes',
+  wss: 'wss://fire-notes.herokuapp.com',
+  api: [
+    { url: '/keys',       verb: 'GET',  what: 'list of keys' },
+    { url: '/notes',      verb: 'POST', what: 'creates/updates a note container' },
+    { url: '/notes/:key', verb: 'GET',  what: 'fetch note container for this key' },
     { wss: {
       request: [ 'GET', 'KEYS', 'POST' ],
-      response:[ 'DATA', 'KEYS', 'ping' ]
+      response: [ 'DATA', 'KEYS', 'ping' ]
     }}
   ]
-};
+}
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+const server = http.createServer(app)
+const serverOnPort = server.listen(port)
 
-app.get('/keys', function (req, res, next) {
+console.log('-- Notes Server listening on port ' + port)
+
+notes.connect(process.env.MONGODB_URI)
+
+const wss = new WsServer({ server: serverOnPort })
+console.log('-- websocket server created')
+
+wss.on('connection', (ws) => {
+  console.log('-- wss: Client connected')
+
+  ws.on('close', () => {
+    console.log('-- wss: Client disconnected')
+  })
+
+  ws.on('message', (message) => {
+    if (message === 'pong') {
+      console.log('-- wss: pong')
+    } else {
+      const obj  = JSON.parse(message)
+      const user = obj.id ? obj.id.toLowerCase() : null
+      console.log('-- wss: ' + obj.type + ' ' + user)
+      if (obj.type === 'GET') {
+        notes.get(user, (err, object) => {
+          if (!err) {
+            if (object) {
+              ws.send(JSON.stringify({ type: 'DATA', id: object.user, values: object.values }))
+            } else {
+              ws.send(JSON.stringify({ type: 'DATA', id: user, values: [] }))
+            }
+          }
+        })
+      }
+      if (obj.type === 'KEYS') {
+        notes.keys((err, keys) => {
+          if (!err) {
+            ws.send(JSON.stringify({ type: 'KEYS', keys: keys }))
+          }
+        })
+      }
+      if (obj.type === 'POST') {
+        notes.post(user, obj.value, (err, object) => {
+          if (!err) {
+            wss.clients.forEach((client) => {
+              client.send(JSON.stringify({ type: 'DATA', id: object.user, values: object.values }))
+            })
+            // NOTE:  if this is a new object, update the clients...
+            if (object.values.length === 1) {
+              notes.keys((err2, keys) => {
+                if (!err2) {
+                  wss.clients.forEach((client) => {
+                    client.send(JSON.stringify({ type: 'KEYS', keys: keys }))
+                  })
+                }
+              })
+            }
+          }
+        })
+      }
+    }
+  })
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/keys', (req, res) => {
   notes.keys((err, keys) => {
     if (!err) {
-      res.json(keys);
+      res.json(keys)
     }
-  });
-});
+  })
+})
 
-app.get('/notes/:username', function (req, res, next) {
-  let user = req.params.username.toLowerCase();
+app.get('/notes/:username', (req, res) => {
+  const user = req.params.username.toLowerCase()
   if (user) {
     notes.get(user, (err, object) => {
       if (!err) {
         if (object) {
-          res.json(object);
-        }
-        else {
-          res.json({ id:user, values:[] });
+          res.json(object)
+        } else {
+          res.json({ id: user, values: [] })
         }
       }
-    });
+    })
   }
-});
+})
 
-app.post('/notes', function (req, res, next) {
-  let user  = req.body.id.toLowerCase();
-  let value = req.body.values;
+app.post('/notes', (req, res) => {
+  const user  = req.body.id.toLowerCase()
+  const value = req.body.values
   notes.post(user, value, (err, object) => {
     if (!err) {
       // NOTE:  push the update to the websocket clients before responding...
       wss.clients.forEach((client) => {
-        client.send(JSON.stringify({ type:'DATA', id:object.user, values:object.values }));
-      });
+        client.send(JSON.stringify({ type: 'DATA', id: object.user, values: object.values }))
+      })
       // NOTE:  if this is a new object, update the clients...
-      if (object.values.length == 1) {
-        notes.keys((err, keys) => {
-          if (!err) {
+      if (object.values.length === 1) {
+        notes.keys((err2, keys) => {
+          if (!err2) {
             wss.clients.forEach((client) => {
-              client.send(JSON.stringify({ type:'KEYS', keys:keys }));
-            });
+              client.send(JSON.stringify({ type: 'KEYS', keys: keys }))
+            })
           }
-        });
+        })
       }
-      res.json(object);
+      res.json(object)
     }
-  });
-});
+  })
+})
 
 app.use((req, res) => {
-  res.send(mock);
-});
-
-let server = http.createServer(app);
-let serverOnPort = server.listen(port);
-
-console.log("-- Notes Server listening on port " + port);
-
-notes.connect(process.env.MONGODB_URI);
-
-let wss = new wsServer({ server: serverOnPort });
-console.log("-- websocket server created");
-
-wss.on('connection', (ws) => {
-  console.log('-- wss: Client connected');
-
-  ws.on('close', () => {
-    console.log('-- wss: Client disconnected');
-  });
-
-  ws.on('message', (message) => {
-    if (message == 'pong') {
-      console.log('-- wss: pong');
-    }
-    else {
-      let obj  = JSON.parse(message);
-      let user = obj.id ? obj.id.toLowerCase() : null;
-      console.log('-- wss: ' + obj.type + ' ' + user);
-      if (obj.type == 'GET') {
-        notes.get(user, (err, object) => {
-          if (!err) {
-            if (object) {
-              ws.send(JSON.stringify({ type:'DATA', id:object.user, values:object.values }));
-            }
-            else {
-              ws.send(JSON.stringify({ type:'DATA', id:user, values:[] }));
-            }
-          }
-        });
-      }
-      if (obj.type == 'KEYS') {
-        notes.keys((err, keys) => {
-          if (!err) {
-            ws.send(JSON.stringify({ type:'KEYS', keys:keys }));
-          }
-        });
-      }
-      if (obj.type == 'POST') {
-        notes.post(user, obj.value, (err, object) => {
-          if (!err) {
-            wss.clients.forEach((client) => {
-              client.send(JSON.stringify({ type:'DATA', id:object.user, values:object.values }));
-            });
-            // NOTE:  if this is a new object, update the clients...
-            if (object.values.length == 1) {
-              notes.keys((err, keys) => {
-                if (!err) {
-                  wss.clients.forEach((client) => {
-                    client.send(JSON.stringify({ type:'KEYS', keys:keys }));
-                  });
-                }
-              });
-            }
-          }
-        });
-      }
-    }
-  });
-});
+  res.send(mock)
+})
 
 setInterval(() => {
   wss.clients.forEach((client) => {
-    client.send('ping');
-  });
-}, 1000);
+    client.send('ping')
+  })
+}, 1000)
--- notes.js
+++ notes.js
@@ -1,69 +1,69 @@
-let mongoose = require('mongoose');
+/* eslint no-console: "off" */
+/* eslint    new-cap: "off" */
+const mongoose = require('mongoose')
 
-let noteSchema = mongoose.Schema({
-    user: String,
-    values: [ String ]
-});
+const noteSchema = mongoose.Schema({
+  user: String,
+  values: [ String ]
+})
 
-let Note = mongoose.model('Note', noteSchema);
-let db   = null;
+const Note = mongoose.model('Note', noteSchema)
+let db   = null
 
-let connectDB = (uri) => {
-  mongoose.connect(uri, { useMongoClient: true });
+const connectDB = (uri) => {
+  mongoose.connect(uri, { useMongoClient: true })
 
-  db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', function() {
+  db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error:'))
+  db.once('open', () => {
     // we're connected!
-    console.log('-- mongoLab:  connected');
-  });
-};
-
-let getKeys = (callback) => {
-  Note.find({}, function(err, list) {
-    if (!err) {
-      let keys = list.map(function(item) { return item.user; }).sort();
-      callback(null, keys);
-    }
-  });
+    console.log('-- mongoLab:  connected')
+  })
 }
 
-let getNote = (user, callback) => {
-  let key = user.toLowerCase();
+const getKeys = (callback) => {
+  Note.find({}, (err, list) => {
+    if (!err) {
+      const keys = list.map((item) => { return item.user }).sort()
+      callback(null, keys)
+    }
+  })
+}
 
-  Note.findOne({ user: key }, callback);
-};
+const getNote = (user, callback) => {
+  const key = user.toLowerCase()
 
-let postNote = (user, value, callback) => {
-  let note = null;
-  let key = user.toLowerCase();
+  Note.findOne({ user: key }, callback)
+}
 
-  getNote(key, function(err, object) {
+const postNote = (user, value, callback) => {
+  let note = null
+  const key = user.toLowerCase()
+
+  getNote(key, (err, object) => {
     if (!err) {
       if (object) {
-        note = object;
+        note = object
 
-        note.values.push(value);
+        note.values.push(value)
+      } else {
+        note = new Note({ user: key, values: [ value ] })
       }
-      else {
-        note = new Note({ user: key, values: [ value ] });
-      }
-      note.save(callback);
+      note.save(callback)
+    } else {
+      callback('failed')
     }
-    else {
-      callback('failed');
-    }
-  });
-};
+  })
+}
 //
 // NOTE:  the callback needs to be in this form - function(error, data)
 //
 //
-let api = {
+const api = {
   connect: connectDB,
   keys: getKeys,
   get: getNote,
   post: postNote
 }
 
-module.exports = api;
+module.exports = api
